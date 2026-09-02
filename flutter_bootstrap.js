25632aa2beded7665abc63ca6fecf8232d59433d
Luis Tellez <luistllz97@gmail.com>
2026-09-02 08:34:22 -0700

    deploy: publish Yokai Admin 9461e55

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3431479770"
+    serviceWorkerVersion: "1312442209"
   }
 });

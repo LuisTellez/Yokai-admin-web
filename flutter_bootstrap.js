ab21d0753bc702a74e59ff684ef9a8f573c98652
Yokai CI <actions@users.noreply.github.com>
2026-09-03 05:07:41 +0000

    deploy: publish source 519ab0a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3334104326" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "245529452" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
